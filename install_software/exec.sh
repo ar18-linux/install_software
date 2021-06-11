ffdf13835af62d2efd3406eee5822fb6cbc150ef
nulysses <adr.zivil@gmail.com>
2021-06-11 14:12:05 +0200

    1

--- install_software/exec.sh
+++ install_software/exec.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+. "${script_dir}/vars"
+
 set -e
-set -x
 
 read -s -p "foo:" foo
-#echo $foo > /tmp/foo
 
 function aur() {
   name="$1"
   rm -rf /tmp/build 
   mkdir /tmp/build
-#  cd /tmp/build
   cd /tmp/build
   git clone https://aur.archlinux.org/${name}.git
-#  git clone "https://aur.archlinux.org/${name}.git"
-#  cd ./alarm-clock-applet
   cd /tmp/build/${name}
   set +e
   LD_PRELOAD= makepkg -m --noconfirm
@@ -35,13 +34,7 @@ function aur() {
     LD_PRELOAD= makepkg --noconfirm
   fi
   set -e
-#  expect -d -c "spawn makepkg -s --noconfirm; expect -re \".*password for \"; send '${foo}';"
-#  LD_PRELOAD= sudo pacman -U --noconfirm
-#  expect -d -c "spawn ls"
   echo "${foo}" | sudo -S -k pacman -U --noconfirm --asdep ./*zst
-  #sudo pacman -U
-#   LD_PRELOAD= expect -c "sudo pacman -U --noconfirm; expect -re \".*password for \"; send '${foo}';"
-#expect -d -c "spawn makepkg -si;"
 }
 
 
@@ -77,25 +70,26 @@ function install_libunique2() {
   echo "${foo}" | sudo -Sk pacman -U --noconfirm libunique-1.1.6-8-x86_64.pkg.tar.zst
 }
 
-#aur alarm-clock-applet
 
-echo "${foo}" | sudo -S -k pacman -Syy
+if [ "${debug}" = "0" ]; then
+  echo "${foo}" | sudo -S -k pacman -Syy
+fi
 
 echo "${foo}" | sudo -S -k pacman -Su --noconfirm
 
 echo "${foo}" | sudo -S -k pacman -Sy --noconfirm \
   spacefm bitwarden firefox thunderbird clementine git expect base-devel postgresql copyq vlc\
-  xfce4-cpugraph-plugin qbittorrent ksysguard xterm lsof hstr
+  xfce4-cpugraph-plugin qbittorrent ksysguard xterm lsof
 
 echo "${foo}" | sudo -S -k usermod -u 5432 postgres
 echo "${foo}" | sudo -S -k groupmod -g 5432 postgres
 
 install_libunique2
-aur "alarm-clock-applet"
 
+aur "alarm-clock-applet"
 aur fsearch-git
 aur masterpdfeditor
 aur google-chrome
+aur hstr
 
-#su - "$(logname)" -c "yay -S pakku"
-#su - "$(logname)" -c "pakku -S alarm-clock-applet"
+set +x
