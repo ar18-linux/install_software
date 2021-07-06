98894309e47a206b2d46172e60506b13e1efe39d
nulysses <adr.zivil@gmail.com>
2021-07-06 22:22:56 +0200

    1

--- install.sh
+++ install.sh
@@ -32,15 +32,14 @@ set -o pipefail
 set -eu
 #################################SCRIPT_START##################################
 
-set -x
+ar18.script.import ar18.script.install
+ar18.script.import ar18.script.execute_with_sudo
 
 . "${script_dir}/vars"
-if [ ! -v ar18_helper_functions ]; then rm -rf "/tmp/helper_functions_$(whoami)"; cd /tmp; git clone https://github.com/ar18-linux/helper_functions.git; mv "/tmp/helper_functions" "/tmp/helper_functions_$(whoami)"; . "/tmp/helper_functions_$(whoami)/helper_functions/helper_functions.sh"; cd "${script_dir}"; export ar18_helper_functions=1; fi
-obtain_sudo_password
 
-ar18_install "${install_dir}" "${module_name}" "${script_dir}"
+ar18.script.install "${install_dir}" "${module_name}" "${script_dir}"
 
-echo "${ar18_sudo_password}" | sudo -Sk chmod +x "${install_dir}/${module_name}/exec.sh"
+ar18.script.execute_with_sudo chmod +x "${install_dir}/${module_name}/exec.sh"
 "${install_dir}/${module_name}/exec.sh" "1"
 
 ##################################SCRIPT_END###################################
